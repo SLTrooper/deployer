e41b055801961603ca2740feed3e08b419c8659c
Stefan <stefan@lorse.me>
2018-02-07 16:27:12 +0100

    new changes

--- deploy.php
+++ deploy.php
@@ -15,7 +15,7 @@
 
 	//echo shell_exec("curl -H 'Authorization: token INSERTACCESSTOKENHERE' -H 'Accept: application/vnd.github.v3.raw' -O -L https://api.github.com/repos/owner/repo/contents/path");
 
-	echo "test";
+	echo "test3";
 
 
 ?>
